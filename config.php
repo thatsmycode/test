296e2f0ef7301ff9bb6e1c666e870ed07e188410
Marc.A <marcalberopascual@gmail.com>
2024-02-05 20:11:37 +0100

    sf

--- config.php
+++ config.php
@@ -1,18 +1,19 @@
 <?php
 
 // Configure session
+/*
 
-//ini_set("session.use_only_cookies", 1);
-//ini_set("session.use_strict_mode", 1);
+ini_set("session.use_only_cookies", 1);
+ini_set("session.use_strict_mode", 1);
 
-/*session_set_cookie_params([
+session_set_cookie_params([
     "lifetime" => 3600,
     "domain" => "testeteste.adaptable.app/index.php",
     "path" => "/",
     "secure" => true,
     "httponly" => true
 ]);
-*/
+
 
 session_start();
 
@@ -31,3 +32,4 @@ if (!isset($_SESSION["last_regeneration"])) {
         $_SESSION["last_regeneration"] = time();
     }
 }
+*/
