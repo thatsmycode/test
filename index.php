2d23e9a69e2d66ac045f2c2cbc560bffc65683e5
Marc.A <marcalberopascual@gmail.com>
2024-02-05 19:25:43 +0100

    l

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 require_once(__DIR__ . "/config.php");
 require_once(__DIR__ . "/utils.php");
 
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 ?>
 
--- utils.php
+++ utils.php
@@ -1,7 +1,9 @@
 <?php
 
 
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
+}
 
 
 class Operation
